5bae5b91bbf053917177496095eb283976d64981
Tyler Dye <tdye@tdye-l.corp.instructure.com>
2018-12-29 12:48:26 -0700

    paused agents?

--- src/components/AdminAgents/AdminAgents.js
+++ src/components/AdminAgents/AdminAgents.js
@@ -6,12 +6,16 @@ export default class AdminAgents extends React.Component {
   render() {
     let adminAvailable = [];
     let adminOnCall = [];
+    let adminPaused = [];
 
     this.props.adminQueue.forEach(agent => {
       if (agent.status === 1) {
         adminAvailable.push(agent);
       }
       if (agent.status === 2) {
+        adminOnCall.push(agent);
+      }
+      if (agent.status === 3) {
         adminOnCall.push(agent);
       }
     });
@@ -42,6 +46,18 @@ export default class AdminAgents extends React.Component {
             {adminOnCall.map(agent => {
               return [
                 <AdminOnCall name={agent.name} callsTaken={agent.callsTaken} />
+              ];
+            })}
+          </table>
+        </div>
+        <div>
+          <table className="calls-paused" id="at-calls-available">
+            {adminPaused.map(agent => {
+              return [
+                <AdminPaused
+                  name={agent.name}
+                  callsTaken={agent.callsTaken}
+                />
               ];
             })}
           </table>
