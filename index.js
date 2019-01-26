ef4be3c5ab6b7718129e8b718625ace98a918747
BlinkVonDoom <blynk.rankin@gmail.com>
2019-01-26 09:18:48 -0700

    index update

--- index.js
+++ index.js
@@ -7,20 +7,10 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/api/passwords", (req, res) => {
-  const count = 5;
-
-  const passwords = Array.from(Array(count).keys()).map(el =>
-    generatePassword(12, false)
-  );
-
-  res.json(passwords);
-
-  console.log(`sent ${count} passwords`);
-});
+// uses fuzeRoutes
+require("./routes/fuzeRoutes")(app);
 
 // catchall handler for any request that doesnt match the above
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
